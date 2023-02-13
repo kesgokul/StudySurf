908083ad5a197a99fd9acd38e8a5d4ad70258710
kesgokul <gokulkesavan5005@gmail.com>
2023-02-13 09:28:21 +0530

    arcana google auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import WhoAreYou from "./pages/WhoAreYou";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import TeacherLogin from "./pages/TeacherLogin";
 import TeacherRegister from "./pages/TeacherRegister";
 import StudentRegister from "./pages/StudentRegister";
-
+import { AuthProvider } from "@arcana/auth";
+import { ProvideAuth } from "@arcana/auth-react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -31,9 +32,13 @@ const router = createBrowserRouter([
     element: <StudentRegister />,
   },
 ]);
+const arcanaAppAddress = import.meta.env.VITE_ARCANA_APP_ADDRESS;
+const provider = new AuthProvider(arcanaAppAddress);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ProvideAuth provider={provider}>
+      <RouterProvider router={router} />
+    </ProvideAuth>
   </React.StrictMode>
 );
--- src/pages/StudentRegister.jsx
+++ src/pages/StudentRegister.jsx
@@ -1,8 +1,10 @@
 import fakeLogo from "/Ellipse.png";
 import fakeProfile from "/profile-pic.png";
 import StudentForm from "../components/StudentForm";
+import { useAuth } from "@arcana/auth";
 
 export default function StudentRegister() {
+  const { user } = useAuth();
   return (
     <div className="w-screen h-screen bg-gradient-to-br from-white to-rose-100 flex flex-col justify-between items-center">
       <div className="mb-auto mt-10 px-4 w-full flex justify-between items-center">
@@ -10,10 +12,14 @@ export default function StudentRegister() {
           <img className=" w-14 h-14" src={fakeLogo} alt="logo" />
           <p className="text-black">Splash Screen</p>
         </div>
-        <img className="w-10 h-10" src={fakeProfile} alt="profile picture" />
+        <img
+          className="w-10 h-10"
+          src={user.picture || fakeProfile}
+          alt="profile picture"
+        />
       </div>
       <h2 className="text-2xl font-semibold">Add you details</h2>
-      <StudentForm />
+      <StudentForm name={user.name} />
       <p className={"mt-auto text-gray-400"}>
         ©Splash Screen | All rights reserved
       </p>
